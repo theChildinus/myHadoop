0467cd727adb14cdf5e70cc1da78881dfe962f28
theChildinus <zky0825@163.com>
2018-07-27 22:27:45 +0800

    first commit about MapReduce

---
+++ src/test/java/MaxTemperatureMapperTest.java
@@ -0,0 +1,41 @@
+import java.io.IOException;
+
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mrunit.mapreduce.MapDriver;
+import org.junit.*;
+
+public class MaxTemperatureMapperTest {
+
+    @Test
+    public void processesValidRecord() throws IOException, InterruptedException {
+        Text value = new Text("0043011990999991950051518004+68750+023550FM-12+0382" +
+                "99999V0203201N00261220001CN9999999N9-00111+99999999999");
+        new MapDriver<LongWritable, Text, Text, IntWritable>()
+                .withMapper(new MaxTemperatureMapper())
+                .withInput(new LongWritable(), value)
+                .withOutput(new Text("1950"), new IntWritable(-11))
+                .runTest();
+    }
+
+    @Ignore
+    @Test
+    public void ignoresMissingTemperatureRecord() throws IOException, InterruptedException {
+        Text value = new Text("0043011990999991950051518004+68750+023550FM-12+0382" +
+                "99999V0203201N00261220001CN9999999N9+99991+99999999999");
+        new MapDriver<LongWritable, Text, Text, IntWritable>()
+                .withMapper(new v2MaxTemperatureMapper())
+                .withInput(new LongWritable(0), value)
+                .runTest();
+    }
+
+    @Test
+    public void processesMalformedTemperatureRecord() throws IOException, InterruptedException {
+        Text value = new Text("0335999999433181957042302005+37950+139117SAO  +0004" +
+                "RJSN V02011359003150070356999999433201957010100005+353");
+        new MapDriver<LongWritable, Text, Text, IntWritable>()
+                .withMapper(new MaxTemperatureMapper())
+                .withInput(new LongWritable(), value)
+                .withOutput(new Text("1957"), new IntWritable(1957))
+                .runTest();
+    }
+}
---
+++ src/test/java/MaxTemperatureTest.java
@@ -0,0 +1,30 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+
+public class MaxTemperatureTest {
+    @Test
+    public void test() throws Exception {
+        Configuration conf = new Configuration();
+        conf.set("fs.defaultFS", "file:///");
+        conf.set("mapreduce.framework.name", "local");
+        conf.setInt("mapreduce.task.io.sort.mb", 1);
+
+        Path input = new Path("/home/kong/code/hadoop-book/input/ncdc/micro");
+        Path output = new Path("output");
+
+        FileSystem fs = FileSystem.getLocal(conf);
+        fs.delete(output, true);
+
+        MaxTemperatureDriver driver = new MaxTemperatureDriver();
+        driver.setConf(conf);
+
+        int exitCode = driver.run(new String[] {input.toString(), output.toString()});
+        assertThat(exitCode, is(0));
+    }
+}
